7dee0d596fe70616112812c6eec54d99b485468c
jayfortier79 <jfortier05@yahoo.com>
2023-10-02 08:11:12 -0500

    exercise api import complete

--- src/Pages/NewOrderPage/NewOrderPage.jsx
+++ src/Pages/NewOrderPage/NewOrderPage.jsx
@@ -9,75 +9,107 @@ const apikey= import.meta.env.VITE_REACT_APP_API_KEY;
 function NewOrderPage() {
   const baseUrl = 'https://api.api-ninjas.com/v1/exercises';
   const headers = { 'X-Api-Key': apikey};
-  const [muscle, setMuscle] = useState('biceps');
-  const [difficulty, setDifficulty] = useState('beginner');
-  const [type, setType] = useState('cardio');
-  const [records, setRecords] = useState([]);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [muscle, setMuscle] = useState('all');
+  const [difficulty, setDifficulty] = useState('all');
+  const [type, setType] = useState('all');
+  const {records, loading, error, refetch} = FetchData(makeUrl(), {headers});
 
   const handleSubmit = async () => {
-    setLoading(true);
-    setError(null);
-
     try {
-      const url = `${baseUrl}?muscle=${muscle}&difficulty=${difficulty}&type=${type}`;
-      const {records: newRecords, error: newError} = await FetchData(url, headers);
-
-      setRecords(newRecords);
-      setError(newError);
+      refetch();
     }catch (err) {
-    setError(err);
-    } finally {
-      setLoading(false);
-    }
-  };
+    console.error(err);
+    } 
+    };
     
   useEffect(() => {
-  handleSubmit();
+  refetch();
 }, []);
+
+function makeUrl() {
+const queryParams = [];
+
+if (muscle !== 'all') {
+  queryParams.push(`muscle=${muscle}`);
+}
+if (difficulty !== 'all') {
+  queryParams.push(`difficulty=${difficulty}`);
+}
+if (type !== 'all') {
+  queryParams.push(`type=${type}`);
+}
+
+const queryString = queryParams.length > 0 ? '?' + queryParams.join('&') : '';
+    return baseUrl + queryString;
+  }
+
+
+const availableMuscles = [
+  'abdominals',
+  'abductors',
+  'adductors',
+  'biceps',
+  'calves',
+  'chest',
+  'forearms',
+  'glutes',
+  'hamstrings',
+  'lats',
+  'lower_back',
+  'middle_back',
+  'neck',
+  'quadriceps',
+  'traps',
+  'triceps',
+];
+
+const availableDifficulties = ['beginner', 'intermediate', 'expert'];
+
+const availableTypes = [
+  'olympic_weightlifting',
+  'plyometrics',
+  'powerlifting',
+  'strength',
+  'stretching',
+  'strongman',
+];
+
+
 
   return (
     <div>
-      <h1>THE EXERCIXE INSTRUCTIONS</h1>
+      <h1>THE EXERCISE INSTRUCTIONS</h1>
         <div>
           <label>Targeted Muscle:</label>
           <select value={muscle} onChange={(e) => setMuscle(e.target.value)}>
-          <option value="abdominals">Abdominals</option>
-          <option value="abductors">Abductors</option>
-          <option value="adductors">Adductors</option>
-          <option value="biceps">Biceps</option>
-          <option value="calves">Calves</option>
-          <option value="chest">Chest</option>
-          <option value="forearms">Forearms</option>
-          <option value="glutes">Glutes</option>
-          <option value="hamstrings">Hamstrings</option>
-          <option value="lats">Lats</option>
-          <option value="lower_back">LowerBack</option>
-          <option value="middle_back">MiddleBack</option>
-          <option value="neck">Neck</option>
-          <option value="quadriceps">Quadriceps</option>
-          <option value="traps">Traps</option>
-          <option value="triceps">Triceps</option>
+          <option value="all">Include All</option>
+          {availableMuscles.map((option) => (
+            <option key={option} value={option}>
+          {option}
+          </option>
+          ))}
           </select>
         </div>
         <div>
         <label>Difficulty Level:</label>
           <select value={difficulty} onChange={(e) => setDifficulty(e.target.value)}>
-          <option value="beginner">Beginner</option>
-          <option value="intermidiate">Intermidiate</option>
-          <option value="expert">Expert</option>
+          <option value="all">Include All</option>
+          {availableDifficulties.map((option) => (
+            <option key={option} value={option}>
+          {option}
+          </option>
+          ))}
           </select>
         </div>
         <div>
         <label>Type of Exercise:</label>
           <select value={type} onChange={(e) => setType(e.target.value)}>
-          <option value="olympic_weightlifting">Olympic Weightlifting</option>
-          <option value="plyometrics">Plyometrics</option>
-          <option value="powerlifting">Powerlifting</option>
-          <option value="strength">Strength Training</option>
-          <option value="stretching">Stretching</option>
-          <option value="strongman">Strongman</option>
+          <option value="all">Include All</option>
+          {availableTypes.map((option) => (
+            <option key={option} value={option}>
+          {option}
+          </option>
+          ))}
           </select>
         </div>
 
@@ -86,14 +118,7 @@ function NewOrderPage() {
       <div>LOADING...</div>
     ) : error ? (
       <div>Error: {error.message}</div>
-
-
-
-
-
-
-    ) : records && records.length > 0 ? (
-      
+   ) : records && records.length > 0 ? (
       <div>
     {records.map((record, index) => (
       <div key={index}>
@@ -108,7 +133,7 @@ function NewOrderPage() {
       </div>
     ))}
   </div>
-  ): (
+  ) : (
     <div>No Data Available</div>
   )}
 </div>
